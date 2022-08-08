feafbc91911a78c815fe030843cd006f803c46bb
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-08 12:19:19 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -31,6 +31,7 @@ sudo systemctl restart tomcat9
 sudo /etc/init.d/guacd start
 #pulseaudio-xrdp
 #tunnel it
+sudo /etc/init.d/guacd start
 wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
 chmod +x cloudflared-linux-amd64
 ./cloudflared-linux-amd64 tunnel --url http://127.0.0.1:8080
