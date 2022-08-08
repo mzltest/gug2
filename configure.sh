d0d07a002612608a73e4984f9463ff4a1a3c54b4
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-08 11:35:49 +0800

    Add files via upload

---
+++ configure.sh
@@ -0,0 +1,36 @@
+# install packages
+apt update
+apt install wget tomcat9 icewm firefox-esr fonts-noto-cjk git gcc make libsndfile1-dev
+
+# dependency for guacamole
+apt install libcairo2-dev libjpeg62-turbo-dev libpng-dev libtool-bin uuid-dev libavcodec-dev libavformat-dev libavutil-dev libswscale-dev freerdp2-dev libssl-dev libvorbis-dev libwebp-dev
+
+#build server
+
+wget https://dlcdn.apache.org/guacamole/1.4.0/source/guacamole-server-1.4.0.tar.gz
+tar -xzf guacamole-server-1.4.0.tar.gz
+cd guacamole-server-1.4.0/
+#sometimes needed --disable-dependency-tracking
+./configure --with-init-dir=/etc/init.d --disable-dependency-tracking
+make
+make install
+ldconfig
+#rdp server installed later
+apt install xrdp
+#adduser /todo:use vars
+echo "mzltest
+mzltest" | adduser "mzltest"
+#mkdir for guacamole
+mkdir /etc/guacamole
+wget -P /etc/guacamole https://raw.githubusercontent.com/mzltest/gug/main/user-mapping.xml
+
+#install client
+wget https://dlcdn.apache.org/guacamole/1.4.0/binary/guacamole-1.4.0.war
+cp guacamole-1.4.0.war /var/lib/tomcat9/webapps/guacamole.war 
+systemctl restart tomcat9
+/etc/init.d/guacd start
+
+#tunnel it
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64
+chmod +x cloudflared-linux-amd64
+./cloudflared-linux-amd64 tunnel --url http://127.0.0.1:8080
