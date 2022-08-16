df8d94b76d4428c30c7ddeaa66261e41fb02d038
mzltest <52124882+mzltest@users.noreply.github.com>
2022-08-16 15:01:39 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,6 +2,4 @@ sudo apt install tomcat9 -y
 sudo service tomcat9 start
 sudo ps -e
 sudo ss -lntp
-sudo ufw allow from any to any port 8080 proto tcp
-sudo ps -e
-sudo ss -lntp
+sudo service tomcat9 status
