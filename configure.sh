b6bc59e06a7e55e6768b603dd6e7eb31a7ca0a24
mzltest <52124882+mzltest@users.noreply.github.com>
2023-03-25 23:39:34 +0800

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -2,7 +2,7 @@
 # 此处为您的脚本
 # install packages
 sudo apt update
-sudo apt install curl wget tomcat9 icewm firefox fonts-noto-cjk git gcc make default-jdk -y
+sudo apt install curl wget tomcat9 gnome-session gdm3 fcitx firefox fonts-noto-cjk git gcc make default-jdk -y
 #non-interactive for keyboards
 echo 'debconf debconf/frontend select Noninteractive' | sudo debconf-set-selections
 sudo apt-get install -y -q
