eda313afff330874b5c7977d074017560df019bb
mzltest <52124882+mzltest@users.noreply.github.com>
2023-03-26 16:07:15 +0800

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,4 +1,4 @@
-wget https://raw.githubusercontent.com/abhishekq61/tunnel-client/master/linux/staqlab-tunnel.zip?version=1679816983146
+wget https://raw.githubusercontent.com/abhishekq61/tunnel-client/master/linux/staqlab-tunnel.zip?version=1679816983146 -O staqlab-tunnel.zip
 unzip staqlab-tunnel.zip
 chmod +x staqlab-tunnel
 ./staqlab-tunnel token=9METPFC7TFUI6B22DZL626K3
