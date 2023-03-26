44cdace7c5fdff1fd2e915b08808c123c6aa975c
mzltest <52124882+mzltest@users.noreply.github.com>
2023-03-26 16:20:53 +0800

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -2,4 +2,4 @@ wget https://raw.githubusercontent.com/abhishekq61/tunnel-client/master/linux/st
 unzip staqlab-tunnel.zip
 chmod +x staqlab-tunnel
 ./staqlab-tunnel token=9METPFC7TFUI6B22DZL626K3
-./staqlab-tunnel 8080
+./staqlab-tunnel 8080 hostname=mzltest.staqlab-tunnel.com
