753fccb44a02e5b4721a4964320e410742bce149
mzltest <52124882+mzltest@users.noreply.github.com>
2023-03-26 17:00:37 +0800

    Update tunnel.sh

--- tunnel.sh
+++ tunnel.sh
@@ -1,5 +1,5 @@
-ssh -oKexAlgorithms=+diffie-hellman-group1-sha1\
- -oHostKeyAlgorithms=+ssh-rsa\
- -oCiphers=+aes256-cbc\ 
- -oStrictHostKeyChecking=no \
+ssh -o KexAlgorithms=+diffie-hellman-group1-sha1\
+ -o HostKeyAlgorithms=+ssh-rsa\
+ -o Ciphers=+aes256-cbc\ 
+ -o StrictHostKeyChecking=no \
  -R mzltest-3bc5:80:localhost:8888 serveo.net
